f3a8fd414a97d31972f3f7789b807ea3023fab5c
biner <heart2icer@outlook.com>
2019-12-22 16:37:05 +0800

    update leetcode-1281-substractproductAndSumSolution

---
+++ src/main/java/subtractProductAndSumSolution.java
@@ -0,0 +1,28 @@
+/**
+ * @author caobin05
+ * @create 2019-12-2019/12/22 2:49 下午
+ * @Overview
+ * @Doc
+ */
+public class subtractProductAndSumSolution {
+
+    // 通过将数值转换为字符串 进行分拆每一位的值
+    public int subtractProductAndSum(int n) {
+        // 将整型数据变为字符串
+        String dataInt = Integer.toString(n);
+        // 初始化积与和
+        Integer sumTotal = 0;
+        Integer productTotal = 1;
+
+        for(int index = 0; index < dataInt.length(); index ++){
+
+            sumTotal += (dataInt.charAt(index) - '0');
+            productTotal *= (dataInt.charAt(index) - '0');
+        }
+        return productTotal - sumTotal;
+    }
+
+
+
+
+}
---
+++ src/test/java/JavaDemo.java
@@ -0,0 +1,12 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.sql.SQLOutput;
+
+public class JavaDemo {
+    public static void main(String[] args){
+        subtractProductAndSumSolution sol = new subtractProductAndSumSolution();
+        System.out.println(sol.subtractProductAndSum(1234));
+
+
+
+    }
+}
